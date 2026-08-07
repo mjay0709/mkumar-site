f2cda9be750726d4f1199bc82b8069ff124fba17
Mrityunjay <idiot.nicc@gmail.com>
2026-08-07 07:32:55 +0530

    Add PostHog analytics, RB2B tracking, and lead funnel events

---
+++ src/scripts/posthog.ts
@@ -0,0 +1,15 @@
+import posthog from "posthog-js";
+
+if (typeof window !== "undefined") {
+  posthog.init("phc_p5kyDoZnVVQyE6cwkn6oKCn3uYg7Taz2YKgU5FwJuKuj", {
+    api_host: "https://us.i.posthog.com",
+    capture_pageview: true,
+    capture_pageleave: true,
+    person_profiles: "identified_only",
+  });
+
+  // Makes debugging easier
+  window.posthog = posthog;
+}
+
+export default posthog;
