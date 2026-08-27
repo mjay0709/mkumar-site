880a666498fc701567b0b695d605005e1f77cc1f
Mrityunjay <idiot.nicc@gmail.com>
2026-08-27 16:49:21 +0530

    Use PostHog reverse proxy

--- src/scripts/posthog.ts
+++ src/scripts/posthog.ts
@@ -2,10 +2,11 @@ import posthog from "posthog-js";
 
 if (typeof window !== "undefined") {
   posthog.init("phc_p5kyDoZnVVQyE6cwkn6oKCn3uYg7Taz2YKgU5FwJuKuj", {
-    api_host: "https://us.i.posthog.com",
+    api_host: "https://v.kumar.xyz",
     capture_pageview: true,
     capture_pageleave: true,
     person_profiles: "identified_only",
+    defaults: '2026-05-30'
   });
 
   // Makes debugging easier
