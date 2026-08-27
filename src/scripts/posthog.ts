4c1f95bcb6c34984e7f923c1d2cf7ae1201f706d
Mrityunjay <idiot.nicc@gmail.com>
2026-08-27 17:06:25 +0530

    Fix PostHog proxy domain

--- src/scripts/posthog.ts
+++ src/scripts/posthog.ts
@@ -2,7 +2,7 @@ import posthog from "posthog-js";
 
 if (typeof window !== "undefined") {
   posthog.init("phc_p5kyDoZnVVQyE6cwkn6oKCn3uYg7Taz2YKgU5FwJuKuj", {
-    api_host: "https://v.kumar.xyz",
+    api_host: "https://v.mkumar.xyz",
     capture_pageview: true,
     capture_pageleave: true,
     person_profiles: "identified_only",
